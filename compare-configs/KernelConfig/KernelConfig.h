45f6a4b45ca77b745fa9a7a295a2e0073f9906bc
JosephBrendler <joseph.brendler@googlemail.com>
2016-11-12 17:30:07 -0500

    This is c++ utility which will compare the configuration files of two different Linux kernels.  CAVEAT: the compare function *half* works. Note that this directory also includes a c++ shared object library called Terminal (which provides much the same screen output formatting for c++ programs that my script_header_joe_brendler provides for bash scripts

---
+++ compare-configs/KernelConfig/KernelConfig.h
@@ -0,0 +1,46 @@
+/***********************************************************************
+ * KernelConfig.h
+ * Joe Brendler
+ * 28 January 2015
+ * class definition --
+ *     ingested linux kernel configuration file and associated utilities
+ **********************************************************************/
+#ifndef KernelConfig_h
+#define KernelConfig_h
+
+#include <ctime>        // time_t, time, ctime
+#include <cstdio>       // printf, etc.
+#include <unistd.h>     // POSIX - usleep
+#include <iostream>     // cout << , etc.
+#include <fstream>	// file operatons, e.g. myFile.close()
+#include <string>	// string operations favored over char[]
+
+#include <sys/ioctl.h>  // a linux header with io controls
+#include <unistd.h>	// a POSIX header
+
+#include "../colorHeader/colorHeader.h"
+#include "../Terminal/Terminal.h"
+
+using namespace std;
+
+class KernelConfig
+{
+  public:
+    KernelConfig( char* name );
+    int test();				// just a test function
+    int load();				// load the parameter array
+    int dump();				// dump the parameter array
+    string parameter[6000];		// the name of a configuration parameter (e.g. "CONFIG_X86_64")
+    string value[6000];			// the value of a configuration parameter (e.g. "y" or "is not set")
+    int length;				// the number of lines in the configuration file
+    int records;			// the number of configuration parameter records loaded
+  private:
+    bool _DEBUG;
+    int _example;
+    char* _filename;
+    string _line;
+    string _msg;
+    Terminal _term;
+};
+
+#endif
---
+++ compare-configs/Terminal/Terminal.h
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Terminal.h
+ * Joe Brendler
+ * 28 January 2015
+ * class definition -- easy (linux) terminal utilities for c++ programs
+ **********************************************************************/
+#ifndef Terminal_h
+#define Terminal_h
+
+#include <string>
+#include <ctime>        // time_t, time, ctime
+#include <cstdio>       // printf, etc.
+#include <unistd.h>     // POSIX - usleep
+#include <iostream>     // cout << , etc.
+
+#include <sys/ioctl.h>  // a linux header with io controls
+#include <unistd.h>   // a POSIX header
+
+#include "../colorHeader/colorHeader.h"
+
+using namespace std;
+
+class Terminal
+{
+  public:
+    Terminal();
+    int width();                          // return terminal width (# columns)
+    int height();                         // return terminal height (# rows)
+    string repeat( char ch, int limit );  // return a string of #limit chars (ch)
+    void separator( string title );       // draw a horizontal line with a simple title
+    bool countdown( int duration );	  // count down #duration seconds
+    void right_status( int status );      // output a boolean status (arg) at the right margin
+    void message ( string msg );          // print message after a green asterisk
+    void E_message ( string msg );        // print out error message after a red asterisk
+    bool summarize_me();                  // print out error message after a red asterisk
+    void CLR();                           // clear the screen
+    void SCP();                           // save the current cursor position
+    void RCP();                           // restore the cursor to the saved position
+    void HCU();                           // Hide the cursor (Note: the trailing character is lowercase L)
+    void SCU();                           // Show the cursor
+    void HVP( int row, int col );         // move cursor to position row, col
+    void CUP( int row, int col );         // move cursor to position row, col
+    void CUU( int reps );                 // Move the cursor up ( #reps cells )
+    void CUD( int reps );                 // Move the cursor down ( #reps cells )
+    void CUF( int reps );                 // Move the cursor forward ( #reps cells )
+    void CUB( int reps );                 // Move the cursor backward ( #reps cells )
+  private:
+    int _cols, _lines;
+  #ifdef TIOCGSIZE
+    struct ttysize _ts;
+  #elif defined(TIOCGWINSZ)
+    struct winsize _ts;
+  #endif /* TIOCGSIZE */
+};
+
+#endif
