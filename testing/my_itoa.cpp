0535f283827dadd530a3fe5a45740d8a0fcbe4cd
JosephBrendler <joseph.brendler@googlemail.com>
2016-11-18 20:44:27 -0500

    added a test of an integer to string conversion function for c++

--- testing/my_itoa.cpp
+++ testing/my_itoa.cpp
@@ -1,16 +1,34 @@
+/*
+ *
+ * my_itoa - convert integer to string test program
+ *
+ *  compiler command:
+ *  $ g++ -o my_itoa my_itoa.cpp
+ */
+
 #include <string>
 #include <iostream>
 #include <cstdio>
 
 using namespace std;
+string itoa( int number )
+{
+  char _alpha[50];
+  int _number = number;
+
+  sprintf(_alpha, "%d", _number);
+  return _alpha;
+}
 
 int main()
 {
-  int i=23;
-  char answer[50];
+  int i=735823;
+  string answer("");
+//  char answer[50];
 
-  sprintf(answer, "%d", i);
+//  sprintf(answer, "%d", i);
 //  itoa(i, answer, 10);
+  answer = itoa(i);
   cout << "itoa returns: " << answer << std::endl;
 
 }
