5092181f2fecec397acc50f8d8980d63ed18a1f9
JosephBrendler <joseph.brendler@googlemail.com>
2024-12-31 23:47:54 -0500

    add init_executables as compromise half update for ckinitramfs

---
+++ mkinitramfs-7.3.1/format-key-card.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# format-key-card.sh
+# joe brendler 2 Jan 15
+# format device specified by argument ($1) as 32bit FAT, of HDD media type (0xf8),
+#   with label="KEY", and random UUID
+source /usr/local/sbin/script_header_brendlefly
+
+BUILD="0.0.0 20150102"
+DEBUG="true"
+#DEBUG="false"
+
+check_argument()
+{
+  if [[ ! $# -eq 1 ]] # correct # of arguments?
+    then E_message "Error: only one argument (device spec) allowed, you gave $#"
+    exit
+  else
+    if [ -b "$1" ]  # specifies a valid block device?
+    then
+      separator "format-key-card-${BUILD}"
+      message "formatting device: $1..."
+    else
+      E_message "Error: invalid block device [ $1 ]"
+      exit
+    fi
+  fi
+}
+
+ask_continue()
+{
+    # prompt to continue
+    # YES = continue to next step, [nN] = stop now (stop now), anything else = ask again
+    continue="x"
+    while [[ ! "$continue" == [YnN] ]]
+    do
+        echo -en $BGon"*"$Boff" type UPPERCASE Y to confirm: " && \
+        read continue && echo
+        # if the response is "g" then set the GO_AHEAD flag and continue; if null apply default (y)
+        [ "$DEBUG" == "true" ] && message "Debug: continue = [ ${continue} ]"
+        case ${continue:0:1} in
+            ""        ) continue="n" ;;
+            "Y"       ) continue="Y" ;;
+            "n" | "N" ) continue="n" ;;
+            *         ) continue="x" ;;
+        esac
+    done
+}
+
+format_card()
+{
+  # format device specified by argument $1
+  my_uuid=$(perl -e 'printf "0x%04X%04X\n", rand 0xFFFF, rand 0xFFFF')
+  command="mkfs.vfat -cv -nKEY -M0xf8 -i$my_uuid -F32 $1"
+  message "About to execute command: [ $command ]"
+  message "${BYon}Are you sure? ${Boff}"
+  ask_continue
+  if [ "$continue" == "Y" ]
+  then
+    mkfs.vfat -cv -nKEY -M0xf8 -i$my_uuid -F32 $1 && message "Command completed successfully."
+  else
+    E_message "Command cancelled."
+  fi
+}
+
+copy_crypt_dat()
+{
+  # copy crypto key to device specified by argument $1
+  message "Would you like to load keyfile to the device?"
+  ask_continue
+  if [ "$continue" == "Y" ]
+  then
+    if [ ! -d "/mnt/KEY" ]  # create mountpoint if needed
+    then
+      message "Creating mountpoint /mnt/Key"
+      mkdir /mnt/KEY
+      [ ! $? -eq 0 ] && E_message "${BRon}failed${Boff}" && exit
+      message "Ok"
+    fi
+    message "Mounting $1 on /mnt/KEY"
+    mount $1 /mnt/KEY
+    [ ! $? -eq 0 ] && E_message "${BRon}failed${Boff}" && exit
+    message "Ok"
+    if [ ! -d "/mnt/KEY/crypt" ]  #create dir if needed
+    then
+      message "Creating keystore directory /mnt/KEY/crypt"
+      mkdir /mnt/KEY/crypt
+      [ ! $? -eq 0 ] && E_message "${BRon}failed${Boff}" && exit
+      message "Ok"
+    fi
+    if [ ! -f "/mnt/KEY/crypt/dat" ]  # copy file if needed
+    then
+      message "Copying keyfile to /mnt/KEY/crypt/dat"
+      continue="Y"
+    else
+      message "/mnt/KEY/crypt/dat already exists.  Overwrite?"
+      ask_continue
+    fi
+    if [ "$continue" == "Y" ]
+    then
+      message "copying crypt/dat to /mnt/KEY/crypt"
+      cp crypt/dat /mnt/KEY/crypt/
+      [ ! $? -eq 0 ] && E_message "${BRon}failed${Boff}" && exit
+      message "Ok"
+    fi
+  else
+    E_message "${BRon}Command cancelled.${Boff}"
+    exit
+  fi
+
+}
+
+#---[ main script ]-------------------------------------
+checkroot
+check_argument $*
+format_card $1
+copy_crypt_dat $1
+echo
+message "All done."
---
+++ mkinitramfs/rotate_initramfs.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# rotate_initramfs
+# joe Brendler 20 Nov 2020
+
+# the GLOBALS file identifies the BUILD, SOURCES_DIR (e.g. /usr/src/initramfs),
+#   and the MAKE_DIR (parent dir of this script)
+source GLOBALS
+source ${SCRIPT_HEADER_DIR}/script_header_brendlefly
+# script header will over-ride BUILD, but globals must be sourced 1st to get _DIRs
+BUILD="${KERNEL_VERSION}-${DATE_STAMP}"
+
+VERBOSE=$TRUE
+verbosity=2
+
+# identify config files
+[[ -e ${MAKE_DIR}/init.conf ]] && CONF_DIR=${MAKE_DIR}
+[[ -e /etc/mkinitramfs/init.conf ]] && CONF_DIR="/etc/mkinitramfs"
+[[ -e ${MAKE_DIR}/mkinitramfs.conf ]] && MAKE_CONF_DIR=${MAKE_DIR}
+[[ -e /etc/mkinitramfs/mkinitramfs.conf ]] && MAKE_CONF_DIR="/etc/mkinitramfs"
+
+# override (ROTATE and verbosity) variables with mkinitramfs.conf file
+source ${MAKE_CONF_DIR}/mkinitramfs.conf
+
+#-----[ initialize local variables ]-------------------------------------------------------
+newinitramfs=""
+newtimestamp=""
+linkname=""
+target=""
+latest=""
+working=""
+safe=""
+
+#-----[ functions ]-----------------------------------------------------------------------
+identify_new_initramfs() {
+  # identify new initramfs
+  #   there should only be one, but examine all and overwrite variable to operate only on the last
+  while read line
+  do
+    newinitramfs="${line}"
+    d_message "newinitramfs: [${newinitramfs}]" 1
+  done <<< $(find . -iname 'initramfs-*' -type f -printf '%Ts\t%p\n' | sort -n | cut -f2)
+
+#  newtimestamp=$(echo ${newinitramfs} | cut -d'-' -f4)
+  newtimestamp="${newinitramfs##*-}"
+  d_echo 1
+  d_message "${BYon}newinitramfs: [${Boff}${newinitramfs}${BYon}]${Boff}" 1
+  d_message "${BYon}newtimestamp: [${Boff}${newtimestamp}${BYon}]${Boff}" 1
+  d_echo 1
+}
+
+identify_current_targets() {
+  # if any exist, identify the current targets of links (latest, working, safe)
+  if [[ $(find . -iname 'initramfs*' -type l -printf '%Ts\t%p\n' | sort -n | cut -f2 ) ]]
+  then
+    while read line
+    do
+      linkname=$(basename $(echo $line | cut -d' ' -f1 | sed 's/://'))
+      target="${line##*link\ to\ }"
+      d_message "linkname: [${LBon}${linkname}${Boff}], target: [${BGon}${target}${Boff}]" 1
+      case $(echo ${linkname} | cut -d'.' -f2) in
+        "latest" ) latest="${target}";;
+        "working" ) working="${target}";;
+        "safe" ) safe="${target}";;
+        * ) echo Error;;
+      esac
+    done <<< $(find . -iname 'initramfs*' -type l -printf '%Ts\t%p\n' | sort -n | cut -f2 | xargs file)
+  fi
+}
+
+makelink() {
+  # create a symbolic link linkname $2 --> target $1
+  local target="$1" linkname="$2"
+  message_n "creating link ${LBon}${linkname}${Boff} --> ${BGon}${target}${Boff} ..."
+  ln -snf ${target} ${linkname}
+  right_status $?
+}
+
+#-----[ main script ]---------------------------------------------------------------------
+checkroot
+separator "rotate_initramfs-${BUILD}"
+
+# move to /boot
+message_n "saving current directory name; cd to /boot/..."
+old_dir=$(pwd) && cd /boot/
+right_status $?
+
+identify_new_initramfs
+identify_current_targets
+
+# rename new initramfs file in .working. format
+message_n "renaming ${BYon}${newinitramfs}${BBon} initramfs.working.${newtimestamp}${Boff} ..."
+mv ${newinitramfs} initramfs.working.${newtimestamp}
+right_status $?
+# reassign newinitramfs variable to the new name
+newinitramfs="initramfs.working.${newtimestamp}"
+
+# put the newest initramfs in rotation and rotate older ones if they exist
+makelink ${newinitramfs} "initramfs.latest"
+[ -f "${latest}" ] && makelink ${latest} "initramfs.working"
+[ -f "${working}" ] && makelink ${working} "initramfs.safe"
+
+# delete the oldest
+[ -f "${safe}" ] && \message_n "removing oldest [${BRon}${safe}${Boff}]" && \
+  ( rm ${safe} ; right_status $? )
+
+message_n "returning to ${old_dir}..."
+cd ${old_dir}
+right_status $?
+
