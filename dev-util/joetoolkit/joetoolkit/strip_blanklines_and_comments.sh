4fd571578aba2037258810232198f42e4fd06d5d
JosephBrendler <joseph.brendler@gmail.com>
2025-11-02 10:31:47 -0500

    add alias cv (clearview) as simple alternative to strip_blanklines_and_comments.sh (and update the latter anyway)
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/joetoolkit/strip_blanklines_and_comments.sh
+++ dev-util/joetoolkit/joetoolkit/strip_blanklines_and_comments.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep -v "#" $1 | while read line; do [ ! -z "${line}" ] && echo $line; done
+#grep -v "#" $1 | while read line; do [ ! -z "${line}" ] && echo $line; done
+grep -Ev '(^\s*$|^#)' "$1"
