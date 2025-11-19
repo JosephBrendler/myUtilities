16b71d81291767aa5f5c1889162e151d1a8cf0b3
JosephBrendler <joseph.brendler@gmail.com>
2025-11-19 12:56:28 -0500

    fix sort uniqe by timestamp for openvpn_dns_updater.sh; bugfix ebup and add pkgdev manifest -f step
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/iptools/openvpn_dns_updater.sh
+++ dev-util/joetoolkit/iptools/openvpn_dns_updater.sh
@@ -12,8 +12,9 @@ openVPN_hosts_file="/etc/hosts.d/20_openVPN_clients"
 
 #-----[ local variables ]------------------
 subnet="192.168.63"
-client_list=()
-ip_list=()
+client_list=()  # client names
+ip_list=()      # client vpn subnet ip address
+ts_list=()      # timestamp: last reference this addr/route
 
 VERBOSE=$TRUE
 [ -z $verbosity ] && verbosity=2  # allow calling script to set this
@@ -33,8 +34,10 @@ read_routing_table() {
     # if captureing and the line starts with target subnet, add the ip and client to lists
     if [ ${CAPTURE} ] && [ "${line:0:${#subnet}}" == "${subnet}" ] ; then
       #Extract IP (field 1) and client name (field 2)
-      local ip=$(echo $line | cut -d',' -f1)
-      local client_raw=$(echo $line | cut -d',' -f2)
+      local ip=$(echo $line | cut -d',' -f1)              # client's virtual address (vpn subnet ip)
+      local client_raw=$(echo $line | cut -d',' -f2)      # client's Common Name (from certificdate)
+      local real_public_ip=$(echo $line | cut -d',' -f3)  # client's public ip & port (we dont need this)
+      local timestamp=$(echo $line | cut -d',' -f4)       # timestamp: last time this route was referenced
 
       # Format client name for local standardization
       # (clients are all issued vpn certificates whose names are reported in openvpn-status.log
@@ -43,8 +46,8 @@ read_routing_table() {
 
       # append to arrays
       ip_list+=("${ip}")
-
       client_list+=("${client_name}")
+      ts_list+=("${timestamp}")
     fi
   done < "${log_file}"
 }
@@ -55,9 +58,16 @@ update_hosts_file() {
 
     # add to temp_file each ip/client_name pair as a nicely formatted hosts file entry
     for ((i=0; i<${#client_list[@]}; i++)); do
-        # put data in 2 columns ip (20 char), client_name (35 char)
-        printf "%-20s %-35s\n" "${ip_list[i]}" "${client_list[i]}"
-    done | LC_COLLATE="en_US.UTF-8" sort -k1.22,2 > "$temp_file"
+        # put data in 2 columns ip (20 char), client_name (20 char), timestamp (20 char)
+        printf "%-20s %-20s %-20s\n" "${ip_list[i]}" "${client_list[i]}" "${ts_list[i]}"
+    done | \
+        # Use awk to de-duplicate by hostname (field 2), keeping only the last (newest) entry seen
+        # LC_COLLATE applied here ensures uppercase Z does not precede lowercase a
+        LC_COLLATE="en_US.UTF-8" awk '{last[$2]=$0} END {for (a in last) print last[a]}' | \
+            # Sort alphabetically by hostname using key/column position (avoid confusing punctuation in col 1)
+            LC_COLLATE="en_US.UTF-8" sort -k1.22,2
+
+#    done | LC_COLLATE="en_US.UTF-8" sort -k1.22,2 -k1.55,3  > "$temp_file"
 #    done | sort -k2.2 > "$temp_file"
 
     # Re-initialize openVPN hosts file with a header
@@ -67,7 +77,7 @@ update_hosts_file() {
     cat "$temp_file" >> "${openVPN_hosts_file}"
 
     # Clean up temporary file
-    rm "$temp_file"
+#    rm "$temp_file"
 
     # Output the newly created hosts file content (optional, for logging/debug)
     cat "${openVPN_hosts_file}"
