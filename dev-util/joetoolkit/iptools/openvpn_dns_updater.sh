2095b32d70dc56c1b6e29e7d1b3e6721f61d99d8
JosephBrendler <joseph.brendler@gmail.com>
2025-11-22 23:38:47 -0500

    update openvpn_dns_updater.sh
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/iptools/openvpn_dns_updater.sh
+++ dev-util/joetoolkit/iptools/openvpn_dns_updater.sh
@@ -10,20 +10,36 @@ PN=$(basename $0)
 log_file=/var/log/openvpn-status.log
 openVPN_hosts_file="/etc/hosts.d/20_openVPN_clients"
 
-#-----[ local variables ]------------------
-subnet="192.168.63"
+#-----[ script "global" variables ]------------------
+ipv4_subnet="192.168.63"
+ipv6_prefix="fd62"
 client_list=()  # client names
 ip_list=()      # client vpn subnet ip address
 ts_list=()      # timestamp: last reference this addr/route
+ip_type_list=() # store 'ipv4' or 'ipv6' for each entry
+
+line=""
+ip_type=""
+ip=""
+client_raw=""
+real_public_ip=""
+timestamp=""
+client_name=""
+
+# regex whitespace patterns
+W1="[[:space:]]+"  # one or more whitespace
+WZ="[[:space:]]*"  # zero or more whitespace
+P1="[[:print:]]+"  # one or more printable characters
+PZ="[[:print:]]*"  # zero or more printable characters
 
 VERBOSE=$TRUE
 [ -z $verbosity ] && verbosity=2  # allow calling script to set this
 
-#-----[ local functions ]-----------------------------
+#-----[ functions ]-----------------------------
 read_routing_table() {
   # read lines of log_file in the client list section only
   CAPTURE=$FALSE
-  local line
+
   while read line
   do
     # if this is the beginning of the routing table section, start capture
@@ -31,56 +47,91 @@ read_routing_table() {
     # if this is the beginning of the global section, stop capture
     [ "${line:0:6}" == "GLOBAL" ] && CAPTURE=${FALSE}
 
-    # if captureing and the line starts with target subnet, add the ip and client to lists
-    if [ ${CAPTURE} ] && [ "${line:0:${#subnet}}" == "${subnet}" ] ; then
+    # if capturing and the line starts with target subnet, add the ip and client to lists
+    if [ ${CAPTURE} ] && ( \
+        [ "${line:0:${#ipv4_subnet}}" == "${ipv4_subnet}" ] || \
+        [ "${line:0:${#ipv6_prefix}}" == "${ipv6_prefix}" ] ) ; then
+
+      # Determine ip address type
+      if [ "${line:0:${#ipv4_subnet}}" == "${ipv4_subnet}" ]; then
+        ip_type="ipv4"
+      else
+        ip_type="ipv6"
+      fi
+
       #Extract IP (field 1) and client name (field 2)
-      local ip=$(echo $line | cut -d',' -f1)              # client's virtual address (vpn subnet ip)
-      local client_raw=$(echo $line | cut -d',' -f2)      # client's Common Name (from certificdate)
-      local real_public_ip=$(echo $line | cut -d',' -f3)  # client's public ip & port (we dont need this)
-      local timestamp=$(echo $line | cut -d',' -f4)       # timestamp: last time this route was referenced
+      ip=$(echo $line | cut -d',' -f1)              # client's virtual address (vpn subnet ip)
+      client_raw=$(echo $line | cut -d',' -f2)      # client's Common Name (from certificdate)
+      real_public_ip=$(echo $line | cut -d',' -f3)  # client's public ip & port (we dont need this)
+      timestamp=$(echo $line | cut -d',' -f4)       # timestamp: last time this route was referenced
 
       # Format client name for local standardization
       # (clients are all issued vpn certificates whose names are reported in openvpn-status.log
       #  each such certificant name has the form "Elrondclient_${client_name}"
-      local client_name=$(echo "${client_raw}" | sed 's/Elrondclient_//' )
+      client_name=$(echo "${client_raw}" | sed 's/Elrondclient_//' )
 
       # append to arrays
       ip_list+=("${ip}")
       client_list+=("${client_name}")
       ts_list+=("${timestamp}")
+      ip_type_list+=("${ip_type}")
     fi
   done < "${log_file}"
 }
 
 update_hosts_file() {
     # Combine client and IP lists, sort uniquely, and write to temporary file
-    local temp_file=$(mktemp)
+    local temp_ipv4=$(mktemp)
+    local temp_ipv6=$(mktemp)
+    local temp_raw=$(mktemp)
 
-    # add to temp_file each ip/client_name pair as a nicely formatted hosts file entry
+    # add all the raw data to temp_file each ip/client_name pair as a nicely tab-separated hosts file entry
     for ((i=0; i<${#client_list[@]}; i++)); do
         # put data in 2 columns ip (20 char), client_name (20 char), timestamp (20 char)
-        printf "%-20s %-20s %-20s\n" "${ip_list[i]}" "${client_list[i]}" "${ts_list[i]}"
-    done | \
-        # Use awk to de-duplicate by hostname (field 2), keeping only the last (newest) entry seen
-        # LC_COLLATE applied here ensures uppercase Z does not precede lowercase a
-        LC_COLLATE="en_US.UTF-8" awk '{last[$2]=$0} END {for (a in last) print last[a]}' | \
-            # Sort alphabetically by hostname using key/column position (avoid confusing punctuation in col 1)
-            LC_COLLATE="en_US.UTF-8" sort -k1.22,2 > "$temp_file"
+        printf "%s\t%s\t%s\t%s\n" "${ip_list[i]}" "${client_list[i]}" "${ts_list[i]}" "${ip_type_list[i]}"
+    done > "$temp_raw"
 
-#    done | LC_COLLATE="en_US.UTF-8" sort -k1.22,2 -k1.55,3  > "$temp_file"
-#    done | sort -k2.2 > "$temp_file"
+    # Process IPv4 Entries (grep picks only ipv4 entries)
+    #   First de-duplicates by hostname (field 2), keeping only the last (newest) entry seen
+    #   LC_COLLATE applied here ensures uppercase Z does not precede lowercase a
+    #   Second awk prints in fixed-width columns appropriate for ipv4 address info
+    grep -E "${W1}ipv4$" "$temp_raw" | \
+        LC_COLLATE="en_US.UTF-8" awk -F '\t' '{last[$2]=$0} END {for (a in last) print last[a]}' |
+        LC_COLLATE="en_US.UTF-8" awk -F '\t' '{ printf "%-20s %-20s %-20s\n", $1, $2, $3 }' | \
+        # Sort alphabetically by hostname. Hostname is now field 2, starting at column 22.
+        LC_COLLATE="en_US.UTF-8" sort -k1.22,2 > "$temp_ipv4"
 
-    # Re-initialize openVPN hosts file with a header
-    echo "# /etc/hosts.d/20_openVPN_hosts" > "${openVPN_hosts_file}"
+    # Process IPv6 Entries (grep picks only ipv6 entries)
+    #   First de-duplicates by hostname (field 2), keeping only the last (newest) entry seen
+    #   LC_COLLATE applied here ensures uppercase Z does not precede lowercase a
+    #   Second awk prints in fixed-width columns appropriate for ipv4 address info
+    grep -E "${W1}ipv6$" "$temp_raw" | \
+        LC_COLLATE="en_US.UTF-8" awk -F '\t' '{last[$2]=$0} END {for (a in last) print last[a]}' |
+        LC_COLLATE="en_US.UTF-8" awk -F '\t' '{ printf "%-40s %-20s %-20s\n", $1, $2, $3 }' | \
+         # Sort alphabetically by hostname. Hostname is now field 2, starting at column 42.
+        LC_COLLATE="en_US.UTF-8" sort -k1.42,2 > "$temp_ipv6"
 
-    # Add the now-sorted and formatted content of the temp file
-    cat "$temp_file" >> "${openVPN_hosts_file}"
+    # Re-initialize openVPN hosts file with a header (IPv4 Section)
+    echo "# /etc/hosts.d/20_openVPN_clients (IPv4 Section)" > "${openVPN_hosts_file}"
+    echo "# IP Address (20)--- Hostname (20)------- Timestamp" >> "${openVPN_hosts_file}"
 
-    # Clean up temporary file
-    rm "$temp_file"
+    # Add the IPv4 content
+    cat "$temp_ipv4" >> "${openVPN_hosts_file}"
+
+    # Add separator and IPv6 header
+    echo -e "\n# ------------------------------------------------------------------" >> "${openVPN_hosts_file}"
+    echo "# /etc/hosts.d/20_openVPN_clients (IPv6 Section)" >> "${openVPN_hosts_file}"
+    echo "# IP Address (40)------------------------ Hostname (20)------- Timestamp" >> "${openVPN_hosts_file}"
+
+    # Add the IPv6 content
+    cat "$temp_ipv6" >> "${openVPN_hosts_file}"
+
+    # Clean up temporary files
+    rm "$temp_ipv4" "$temp_ipv6" "$temp_raw"
 
     # Output the newly created hosts file content (optional, for logging/debug)
     cat "${openVPN_hosts_file}"
+
 }
 
 #-----[ main script ]----------------------------------------
