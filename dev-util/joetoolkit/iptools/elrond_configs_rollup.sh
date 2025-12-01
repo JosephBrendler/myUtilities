d3312aeac4a75dfc82f44750296b73958ddcd63c
JosephBrendler <joseph.brendler@gmail.com>
2025-12-01 09:36:08 -0500

    update elrond_configs_rollup.sh
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/iptools/elrond_configs_rollup.sh
+++ dev-util/joetoolkit/iptools/elrond_configs_rollup.sh
@@ -13,6 +13,7 @@ configs=(
 /etc/shorewall6/rules /etc/shorewall6/policy
 /etc/shorewall/snat /etc/shorewall/interfaces /etc/shorewall/zones
 /etc/shorewall/rules /etc/shorewall/policy
+/etc/conf.d/chronyd /etc/chrony.conf
 /etc/dnsmasq.conf /etc/openvpn/server.conf /etc/hosts /etc/conf.d/net
 /etc/radvd.conf /etc/dhcpcd.conf /etc/resolv.conf /etc/stubby/stubby.yml
 /etc/ulogd.conf /etc/syslog.conf /etc/logrotate.conf
@@ -20,7 +21,13 @@ configs=(
 /etc/ssh/sshd_config.d/9999999gentoo-pam.conf /etc/ssh/sshd_config.d/9999999gentoo-subsystem.conf
 /etc/ssh/sshd_config.d/9999999gentoo.conf /etc/conf.d/net /etc/conf.d/apache2
 /etc/conf.d/distccd /etc/conf.d/dnsmasq /etc/conf.d/shorewall /etc/conf.d/shorewall6
+/etc/hostapd/hostapd.conf /etc/conf.d/hostapd
+/etc/rsyncd.conf /etc/conf.d/rsyncd
+/etc/openvpn/server.conf /etc/conf.d/openvpn
+/etc/haveged.conf /etc/conf.d/haveged
 /etc/conf.d/stubby /etc/conf.d/node_exporter /etc/conf.d/prometheus
+/etc/prometheus/prometheus.yml /etc/sysconfig/node_exporter
+/etc/sysstat /etc/sysstat.ioconf
 )
 
 timestamp() {
