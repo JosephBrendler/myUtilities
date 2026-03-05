2371784b96e21ede3859a4428f2ebf6803c9aa09
JosephBrendler <joseph.brendler@gmail.com>
2026-03-05 14:28:44 -0500

    script_header_joetoo-0.5.0.tbz2 unified messaging/logging system to include unicode emoji and box drawing support
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/script_header_joetoo/print_all_unicode.sh
+++ dev-util/script_header_joetoo/print_all_unicode.sh
@@ -21,13 +21,14 @@ grep -Ev "^(\s*$|#)" /usr/sbin/script_header_joetoo_unicode | while read -r _ite
     # get the raw \Uxxxx value from the environment
     eval "_raw_val=\"\$$_name\""
     # cook the hex into a binary character using your existing logic
-    _cooked_val=$(u_hex_to_octal "$_raw_val")
+#    _octal_val=$(u_hex_to_octal "$_raw_val")
+    _octal_val=$(emit_utf8_from_hex "$_raw_val")
     if [ $((_i % 2)) -eq 0 ]; then _clr="${BCon}"; else _clr="${BGon}"; fi
     # print the label and the rendered glyph
     _pad=$(repeat '.' $(( _column_width - ${#_name} -2 )))
     printf "${_clr}%s${_clr}%s${Boff}" "$_name" "${_pad}: "
-    LC_CTYPE=en_US.utf8 printf '%b\n' "$_cooked_val"
+    LC_CTYPE=en_US.utf8 printf '%b\n' "$_octal_val"
     _i=$(( _i + 1 ))
 done
 # clean up local loop variables
-unset _name _names _raw_val _cooked_valdone
+unset _name _names _raw_val _octal_valdone
