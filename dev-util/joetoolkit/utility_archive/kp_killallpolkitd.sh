6c6af902d83417e03b4e7e4f875960bfcdd15768
JosephBrendler <joseph.brendler@gmail.com>
2026-03-19 18:43:29 -0400

    joetoolkit-0.8.2.tbz2 - closed utility_archive back up as a tarball; decided NOT to consolidate script_header_joetoo
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/utility_archive/kp_killallpolkitd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while [[ "1" ]]; do killall polkitd; sleep 0.1; done
--- dev-util/joetoolkit/utility_archive/strip_blanklines_and_comments.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#grep -v "#" $1 | while read line; do [ ! -z "${line}" ] && echo $line; done
-grep -Ev '(^\s*$|^#)' "$1"
