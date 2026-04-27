e0cb1c0f3548bc80455cb698f7290d9976b7345a
JosephBrendler <joseph.brendler@gmail.com>
2026-04-27 11:59:08 -0400

    joetoolkit-0.8.16.tbz2 - update joetoolkit/router/elrond_configs_rollup.sh
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/router/elrond_configs_rollup.sh
+++ dev-util/joetoolkit/router/elrond_configs_rollup.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
-source /usr/sbin/script_header_joetoo
+
+script_header_installed_dir="/usr/sbin"
+#script_header_installed_dir=/home/joe/myUtilities/dev-util/script_header_joetoo
+header="${script_header_installed_dir%/}/script_header_joetoo"
+echo "header: $header"
+
+# source script_header
+# shellcheck source="/usr/sbin/script_header_joetoo"
+if [ -f "$header" ]; then
+  # source it if it isn't already sourced
+  # (redundant since it should also make this same check at its top section)
+  if [ "$JOETOO_ENVIRONMENT_ALREADY_SOURCED" ]; then
+    source_finding="was already sourced"
+  else
+    . "$header"
+ #   . "${header}_ssh"   # also source the ssh module
+    source_finding="sourced"
+  fi
+else
+  printf '%s' "failed to source header; cannot continue"
+  exit 1
+fi
+# verify sourced script_header
+printf '%s' "Checking for header commands to confirm sourcing"
+if command -v toc >/dev/null 2>&1 && command -v run_sequence >/dev/null 2>&1 ; then
+  printf '%s\n' " success; header ${source_finding}"
+else
+  printf '%s\n' " Failed; header not sourced; cannot continue"; exit 1
+fi
+
+# verify sourced script_header ssh key management module
+#printf '%s' "Checking for ssh header commands to confirm sourcing"
+#if command -v load_key >/dev/null 2>&1 && command -v run_sequence >/dev/null 2>&1 ; then
+#  printf '%s\n' " success; ssh header ${source_finding}"
+#else
+#  printf '%s\n' " Failed; ssh header not sourced; cannot continue"; exit 1
+#fi
+
+# note: considered sourcing script_header_joetoo_ssh, but there is not benefit for this script
+#   ssh agent will ask for password when script runs scp - (same experience if using
+#   script_header_joetoo_ssh start -> load_key -> unload_key -> stop (but with more overhead)
 
 user=joe
 win_user=joebr
 rollup_file="/home/${user}/script/elrond_configs_rollup"
-#target="${user}@gmki91:/home/${user}/"
-target="${win_user}@joelaptop:C:/Users/${win_user}/Desktop/"
+target="${user}@gmki91:/home/${user}/"
+#target="${win_user}@joelaptop:C:/Users/${win_user}/Desktop/"
+
+#SSH_KEY="/home/${user}/.ssh/id_rsa"
+
+#bremoji $wrench
+#j_msg -${notice} -p "starting ssh agent ..."
+#start_ssh_agent; handle_result $? "" "" -$notice || die "failed to start_ssh_agent"
 
 configs=(
 /etc/shorewall6/snat /etc/shorewall6/interfaces /etc/shorewall6/zones
@@ -38,10 +84,35 @@ for x in "${configs[@]}" ; do
   grep -Ev --color=auto "(^\s*$|^#)" 2>/dev/null $x;
 done >> "${rollup_file}" || die "failed to roll up configs"
 
+text2pdf "${rollup_file}"
 cat "${rollup_file}"
+
+
+#bremoji $wrench
+#j_msg -${notice} -p "loading ssh key ..."
+#load_key; handle_result $? "" "" -$notice || die "failed to load_key"
+
+# source environment variables to guarantee persistence
+#if [ -f "${AGENT_ENV_FILE}" ]; then
+#  j_msg -${notice} -p -n "sourcing AGENT_ENV_FILE..."
+#  source "${AGENT_ENV_FILE}"
+#  right_status $? ${notice} || die "failed to source AGENT_ENV_FILE" 1
+#else
+#  j_msg -${err} "AGENT_ENV_FILE not found"
+#  return 2
+#fi
 
 bremoji $hammer
 j_msg -${notice} -p "now transferring ${rollup_file} to ${target} with scp ..."
-sudo -u ${user} scp "${rollup_file}" "${target}"  || die "failed to scp rollup to ${target}"
+#sudo -u ${user} scp "${rollup_file}"{,.pdf} "${target}"  || die "failed to scp rollup to ${target}"
+sudo -u "${user}" rsync -av --exclude="*.sh" "${rollup_file}"* "${target}" || die "failed to rsync files"
+
+#bremoji $wrench
+#j_msg -${notice} -p "unloading ssh key ..."
+#unload_key; handle_result $? "" "" -$notice || die "failed to unload_key"
+
+#bremoji $wrench
+#j_msg -${notice} -p "stopping ssh agent ..."
+#stop_ssh_agent; handle_result $? "" "" -$notice || die "failed to stop_ssh_agent"
 
 j_msg -${notice} -p -M $idx_face_beam "${BGon}Success${Boff}"
