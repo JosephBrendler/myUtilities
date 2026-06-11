920c7fde8d491059892a7a82ce6ea0e8175af4d0
JosephBrendler <joseph.brendler@gmail.com>
2026-06-11 14:38:11 -0400

    joetoolkit-0.8.36.tbz2 - add joetoolkit/iptools/wifi_debug_watch and update joetoolkit/joetoolkit/clear_gnupg_locks.sh
    Signed-off-by: JosephBrendler <joseph.brendler@gmail.com>

--- dev-util/joetoolkit/joetoolkit/clear_gnupg_locks.sh
+++ dev-util/joetoolkit/joetoolkit/clear_gnupg_locks.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
-
+# shellcheck source=/usr/sbin/script_header_joetoo
 source /usr/sbin/script_header_joetoo
 
 checkroot
 
 PN=${0##*/}
+if [ -f /etc/joetoolkit/BUILD ]; then . /etc/joetoolkit/BUILD; else BUILD="0.0.0"; fi
 
 signing_keyring_dir="/root/.gnupg"
+signing_key_email="joseph.brendler@gmail.com"
+
+# to read your signing keygrip, do e.g. -
+# gpg --homedir="${signing_keyring_dir}" --with-keygrip -K "${signing_key_email}"
+# (note: pick the "sec" keygrip, not the "ssb"
+signing_keygrip="661F32EE3F3EC59ABC93730764EB3948A3A034E1"
+
 
 signing_pubkey_dir="${signing_keyring_dir%/}/public-keys.d"
 signing_lock="${signing_pubkey_dir%/}/pubring.db.lock"
+
+separator "$(hostname)" "${PN}-${BUILD}"
+
+# the lockout this script corrects is usually caused by stale keyboxd, gpg, or gpg-agent processes
+# holding a lock on the signing keys ...
+# if there are gpg, keyboxd, or gpg-agent processes, kill them
+# (keyboxd and gpg-agent are transient daemons managed completely on-demand by GnuPG,
+#  it is safe to forcefully terminate them when no cryptographic tasks are actively processing;
+#  they will cleanly respawn on next package build)
+# (-9 causes this to use SIGKILL - more forcefull vs SIGTERM w/o -9)
+for x in keyboxd gpg-agent gpg; do
+  if pgrep "$x" &>/dev/null; then
+    j_msg "-${notice}" -p -n "running killall $x"
+    killall -9 "$x" 2>/dev/null
+    right_status $? "$notice"
+  else
+    j_msg "-${notice}" -p "no [$x] processes running"
+  fi
+  milli_sleep 150
+done
 
 # if the signing keyring is locked, remove the lock(s)
 if [ -f "${signing_lock}" ] ; then
@@ -23,3 +51,21 @@ if [ -f "${signing_lock}" ] ; then
 else
     j_msg "-${notice}" -p "Signing keyring is ${BGon}not locked${Boff}"
 fi
+
+# verify lock(s) cleared
+j_msg "-${notice}" -p -n "Verifying signing key is unlocked"
+gpg --homedir "${signing_keyring_dir}" --list-keys > /dev/null
+right_status $? "$notice"
+
+# now unlock the keyring for upcoming emerge operations (joetoo_script_header ask_pass)
+j_msg "-${notice}" -p -n "Re-starting gpg-agent"
+gpg-connect-agent reloadagent /bye &>/dev/null
+right_status $? "$notice"
+
+j_msg "-${notice}" -p "Presetting cached passphrase ..."
+pass_phrase="$(ask_pass "    Enter signing keyring passphrase: ")"
+printf '%s\n' "$pass_phrase" | /usr/libexec/gpg-preset-passphrase --preset "${signing_keygrip}"
+j_msg "-${notice}" -p "Done presetting cached passphrase; result:"
+handle_result $? '' '' "$notice"
+
+j_msg "-${notice}" -m "Done"
